2bb93c6b22c000328c67011252e7ad5b3e13110f
JagguJagadeesh <jaggujagadeesh447@gmail.com>
2025-02-21 14:24:29 +0530

    setting up project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
+      <Button>hello</Button>
+    </div>
+  );
+}
